8f9d801d211b2f11bb7074c9f3031d620d314085
argally <amandargalligan@gmail.com>
2024-11-27 16:59:16 +0000

    fix: configure master kube-proxy

--- multipass/master.tf
+++ multipass/master.tf
@@ -21,5 +21,18 @@ resource "null_resource" "master-node" {
 echo ${data.external.master[count.index].result.ip} master-${count.index} >> /tmp/hosts_ip.txt
 CMD
   }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo '*** Bind Kube-proxy 0.0.0.0 Metrics address:'"
+      "kubectl -n kube-system get cm kube-proxy -o yaml |sed 's/metricsBindAddress: ""/metricsBindAddress: "0.0.0.0:10249"/' | kubectl apply -f -"
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "kubectl -n kube-system patch ds kube-proxy -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"updateTime\":\"`date +'%s'`\"}}}}}"
+    ]
+  }
   count = 1
 }
