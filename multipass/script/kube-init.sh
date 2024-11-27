a8d3c9c9cce5596786b2ad0d48fd5ca5ef7a0a1b
argally <amandargalligan@gmail.com>
2024-11-27 14:42:31 +0000

    fix: ensure probes bind correct address adn restart ntp service

--- multipass/script/kube-init.sh
+++ multipass/script/kube-init.sh
@@ -35,5 +35,11 @@ fi
 sleep 5
 echo "*** Bind API to 0.0.0.0 address:"
 sed -i -e 's/--bind-address=127.0.0.1/--bind-address=0.0.0.0/' /etc/kubernetes/manifests/kube-scheduler.yaml
+sed -i -e 's/host: 127.0.0.1/host: 0.0.0.0/' /etc/kubernetes/manifests/kube-scheduler.yaml
 sed -i -e 's/127.0.0.1:2381/0.0.0.0:2381/' /etc/kubernetes/manifests/etcd.yaml
 sed -i -e 's/--bind-address=127.0.0.1/--bind-address=0.0.0.0/' /etc/kubernetes/manifests/kube-controller-manager.yaml
+sed -i -e 's/host: 127.0.0.1/host: 0.0.0.0/' /etc/kubernetes/manifests/kube-controller-manager.yaml
+sleep 5
+echo "*** Bind Kube-proxy 0.0.0.0 Metrics address:"
+kubectl -n kube-system get cm kube-proxy -o yaml |sed 's/metricsBindAddress: "0.0.0.0:10248"/metricsBindAddress: "0.0.0.0:10249"/' | kubectl apply -f -
+kubectl -n kube-system patch ds kube-proxy -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"updateTime\":\"`date +'%s'`\"}}}}}"
